f55044ad21dc3ebb46d24482dac7c5c3ac1b48d3
Bryan McAfee <brymcafee@gmail.com>
2018-05-22 16:36:58 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,15 +5,5 @@ return "Hello, my name is #{name}."
 end
 
 def batch_badge_creator(attendees)
-  attendees.map do |attendee|
-    "Hello, my name is #{attendee}."
+  attendees.collect {|name| badge_maker(name)}
 end
-end
-
-def assign_rooms(attendees)
-  attendees each_with_index.map do |attendee, index|
-    "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
-  end
-end
-
-  
