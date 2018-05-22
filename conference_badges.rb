1c46510bd4be3e7a3e863a0ed9343da1071fc76c
Bryan McAfee <brymcafee@gmail.com>
2018-05-22 16:42:15 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,3 +11,13 @@ end
 def assign_rooms(attendees)
   attendees.each_with_index.map {|attendees, index| "Hello, #{name}! You'll be assigned to room #{index+1}!"}
 end
+
+def printer(speakers)
+ batch_badge_creator(speakers).each do |badge|
+  puts badge
+ end
+ 
+ assign_rooms(speakers).each do |assignment|
+  puts assignment
+ end
+end
